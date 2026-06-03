1bfa2448e76f5bceeeb0b99ddf754b31dc31cb47
asd841018 <asd841018@gmail.com>
2026-06-03 09:14:34 +0000

    docling for pdf parse

--- src/rag/parse.py
+++ src/rag/parse.py
@@ -1,21 +1,25 @@
 import os
 from dotenv import load_dotenv
-from markitdown import MarkItDown
-from openai import OpenAI
-
+# from markitdown import MarkItDown
+# from openai import OpenAI
+from docling.document_converter import DocumentConverter
 
 load_dotenv()
 
-md = MarkItDown(
-    enable_plugins=True,
-    llm_client=OpenAI(
-        api_key=os.environ.get("OPENAI_API_KEY"),
-    ),
-    llm_model="gpt-4o",
-)
+source = "/home/asd841018/mini-agent/.assets/91ba1d46cdde9c1c0cf34f6bcc107741244f8f3d.pdf"
+converter = DocumentConverter()
+result = converter.convert(source)
+print(result.document.export_to_markdown())
+# md = MarkItDown(
+#     enable_plugins=True,
+#     llm_client=OpenAI(
+#         api_key=os.environ.get("OPENAI_API_KEY"),
+#     ),
+#     llm_model="gpt-4o",
+# )
 
-result = md.convert("/Users/jerry/Downloads/pdf_reports/91ba1d46cdde9c1c0cf34f6bcc107741244f8f3d.pdf")
-with open("output.md", "w", encoding="utf-8") as file:
-    file.write(result.text_content)
+# result = md.convert("/home/asd841018/mini-agent/.assets/91ba1d46cdde9c1c0cf34f6bcc107741244f8f3d.pdf")
+# with open("output.md", "w", encoding="utf-8") as file:
+    # file.write(result.text_content)
 # result.save_as_markdown("./test.md")
 # result.markdown
